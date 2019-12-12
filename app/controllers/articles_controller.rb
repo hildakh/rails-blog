0b9beeeb6517832b292a832e416e7d2f2e291a36
hildakh <h_khajehamiri@yahoo.com>
2019-12-12 03:52:05 +0000

    update action updated to redirect to article page or to the edit page in case of any errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,15 +24,20 @@ class ArticlesController < ApplicationController
     else 
       render 'new'
     end
+  end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
-
-  def update
-  end
 
 end
