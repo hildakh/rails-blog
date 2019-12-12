2c0173c428c68823c0f6aa9aef75cbaea801253f
hildakh <h_khajehamiri@yahoo.com>
2019-12-12 03:23:22 +0000

    fixed the validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,4 @@
 class Article < ActiveRecord::Base
-  class Article < ActiveRecord::Base
-    validates :title, presence: true,
-                      length: { minimum: 5 }
-  end
+  validates :title, presence: true,
+                    length: { minimum: 5 }
 end
