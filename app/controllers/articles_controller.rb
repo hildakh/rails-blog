638b5343c7b3aaa875a0fd0422b5876cc894117c
hildakh <h_khajehamiri@yahoo.com>
2019-12-12 03:20:09 +0000

    else added to take care of failed save

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,12 +3,13 @@ class ArticlesController < ApplicationController
   def index 
     @articles = Article.all
   end
-  
+
   def show 
     @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
@@ -17,8 +18,12 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
+    if @article.save
     redirect_to @article
+    else 
+      render 'new'
+    end
+    
   end
 
   private
